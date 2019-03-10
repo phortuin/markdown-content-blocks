dc8b68e510095fe2aa20a752fe13e80770db50e4
Anne Fortuin <phortuin@gmail.com>
2019-03-10 10:22:37 +0100

    test: write test for getFiles

--- test.js
+++ test.js
@@ -26,7 +26,17 @@ Here be text
 ![](https://i.imgur.com/p66zLsr.jpg)
 `
 
+const fileList = [
+	'example.js',
+	'moretext.txt',
+]
+
 test('Parses markdown', t => {
 	t.plan(1)
 	t.equal(contentBlocks(source, files), output)
+})
+
+test('Gets required file list', t => {
+	t.plan(1)
+	t.deepEqual(contentBlocks.getFiles(source), fileList)
 })
